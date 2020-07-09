0a3cf228521f47d5c9b3ad03bb6fbc4cbd47eb48
Ajay Kharat <65547863+iamajaykharat@users.noreply.github.com>
2020-07-09 14:38:30 +0530

    Update settings.py

--- ownzone/settings.py
+++ ownzone/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '%xv=do5x40(&i$i$f$_dk5n1bv6ygllt59haksu7v3-yuwn4w_'
+SECRET_KEY = '-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
